14107a961f66561ce67c5fbecbe5b5b3c0d0346c
wuzhengyi <18805156360@163.com>
2018-04-17 10:25:46 +0800

    [hw 2] use gradAscent

--- Homework/hw2/LR_main/LR_main.py
+++ Homework/hw2/LR_main/LR_main.py
@@ -16,6 +16,7 @@ def loadTrainDataSet():
     dataMat = numpy.c_[dataMat, ones]
     return dataMat, labelMat
 
+
 def loadTestDataSet():
     '创建训练数据集，分类标签集并返回。'
     dataMat = []
@@ -32,16 +33,9 @@ def loadTestDataSet():
     dataMat = numpy.c_[dataMat, ones]
     return dataMat, labelMat
 
-# =====================================
-# 输入：
-#        x: w^Tx+b
-# 输出:
-#       p1
-# =====================================
 
 def p1(x):
-    val = numpy.exp(-x)
-    return 1.0 / (1 + val)
+    return 1.0 / (1 + numpy.exp(-x))
 
 
 def firstDerivative(weight, dataMatrix, labelMat):
@@ -60,7 +54,8 @@ def secondDerivative(weight, dataMatrix):
     # 初始化结果矩阵11*1
     result = numpy.zeros((dataMatrix.shape[1], dataMatrix.shape[1]))
     for i in range(dataMatrix.shape[0]):
-        result += dataMatrix[i].transpose() * dataMatrix[i] * p1(dataMatrix[i] * weight).A1[0] * (1 - p1(dataMatrix[i] * weight).A1[0])
+        result += dataMatrix[i].transpose() * dataMatrix[i] * p1(dataMatrix[i] * weight).A1[0] * (
+                    1 - p1(dataMatrix[i] * weight).A1[0])
     return numpy.mat(result)
 
 
@@ -71,66 +66,68 @@ def secondDerivative(weight, dataMatrix):
 # 输出:
 #        weights: 最佳拟合参数向量
 # =====================================
-def newton(dataMatIn, classLabels):
+def newton(trainFeature, trainLabel):
     '基于牛顿法的logistic回归分类器'
 
     # 将数据集，分类标签集存入矩阵类型。
-    dataMatrix = numpy.mat(dataMatIn)
-    labelMat = numpy.mat(classLabels).transpose()
+    feature = numpy.mat(trainFeature)
+    label = numpy.mat(trainLabel).transpose()
 
     # 初始化回归参数向量
-    m, n = numpy.shape(dataMatrix)
+    m, n = numpy.shape(feature)
     weights = numpy.mat(numpy.zeros((n, 1)))
     dert = numpy.inf
 
     # 对回归系数进行牛顿迭代
-    while dert > numpy.exp(-3):
-        f = firstDerivative(weights,dataMatrix,labelMat)
-        s = secondDerivative(weights, dataMatrix)
-        tempWeights = weights - numpy.linalg.pinv(s) * f
-        dert = abs(sum((tempWeights - weights).A1))
-        weights = tempWeights
+    tempWeights = numpy.mat(numpy.ones((n, 1)))
+    times = 0
+    # while dert > numpy.exp(-10):
+    while dist(weights, tempWeights) > numpy.exp(-16):
+    # while times < 20:
+        times += 1
+        f = firstDerivative(weights, feature, label)
+        s = secondDerivative(weights, feature)
+        tempWeights = weights
+        # tempWeights = weights - numpy.linalg.pinv(s) * f
+        # dert = abs(sum((tempWeights - weights).A1))
+        weights = weights - numpy.linalg.pinv(s) * f
+    print(times)
     return weights
 
 
-def gradAscent(dataMatIn, classLabels):
-    '基于梯度上升法的logistic回归分类器'
+def gradAscent(trainFeature, trainLabel):
+    feature = numpy.mat(trainFeature)
+    label = numpy.mat(trainLabel).transpose()
+    weights = numpy.ones((feature.shape[1], 1))
 
-    # 将数据集，分类标签集存入矩阵类型。
-    dataMatrix = numpy.mat(dataMatIn)
-    labelMat = numpy.mat(classLabels).transpose()
-
-    # 上升步长度
-    alpha = 0.001
-    # 迭代次数
-    maxCycles = 500
-    # 初始化回归参数向量
-    m, n = numpy.shape(dataMatrix)
-    weights = numpy.ones((n, 1))
-
-    # 对回归系数进行maxCycles次梯度上升
-    for k in range(maxCycles):
-        h = p1(dataMatrix * weights)
-        error = (labelMat - h)
-        weights = weights + alpha * dataMatrix.transpose() * error
+    for k in range(100):
+        h = p1(feature * weights)
+        error = (label - h)
+        weights = weights + 0.01 * feature.transpose() * error
 
     return weights
 
-def test(num):
-    '测试'
+
+def dist(vec1, vec2):
+    dist = numpy.linalg.norm(vec1 - vec2)
+    return dist
+
+def classify(num):
+
     dataArr, labelMat = loadTrainDataSet()
     trainLabel1 = labelMat
-    t = 0; f = 0
+
     for index, value in enumerate(trainLabel1):
         if value != num:
             trainLabel1[index] = 0
-            f+=1
-        else:
-            t+=1
-    r = t/f
-    # print(r)
-    weights = newton(dataArr, labelMat)
-    tp = 0; fp = 0; fn = 0; tn = 0;
+
+    # weights = newton(dataArr, labelMat)
+    weights = gradAscent(dataArr, labelMat)
+
+    tp = 0;
+    fp = 0;
+    fn = 0;
+    tn = 0;
     for i in range(dataArr.shape[0]):
         if p1(dataArr[i] * weights) > 0.5:
             if labelMat[i] == num:
@@ -138,32 +135,41 @@ def test(num):
             else:
                 fp += 1
         else:
-            if labelMat[i]==num:
+            if labelMat[i] == num:
                 fn += 1
             else:
                 tn += 1
-    print(tp,fn,fp,tn)
-    print("正例：", num, "查准率：", tp/(tp+fp), "查全率：", tp/(tp+fn))
-
-
-
-def newTest():
-    trainFeature=numpy.genfromtxt("./testSet.txt")
-    trainLabel=trainFeature[:,2]
-    trainFeature=trainFeature[:,0:-1]
-    for i in range(trainFeature.shape[1]):
-        mins = numpy.min(trainFeature[:, i])
-        maxs = numpy.max(trainFeature[:, i])
-        for j in range(trainFeature.shape[0]):
-            trainFeature[j][i] = (trainFeature[j][i]-mins)/(maxs-mins)
-    ones = numpy.ones(trainFeature.shape[0])
-    dataMat = numpy.c_[trainFeature, ones]
-    weights = newton(dataMat, trainLabel)
-    print(weights)
+    # print(tp, fn, fp, tn)
+    # print("正例：", num, "查准率：", tp / (tp + fp), "查全率：", tp / (tp + fn))
+    return weights
 
 if __name__ == '__main__':
-    test(1)
-    test(2)
-    test(3)
-    test(4)
-    test(5)
+
+    feature, label = loadTestDataSet()
+    label_dict = {}
+    for l in label:
+        if l not in label_dict:
+            label_dict[l] = 1
+        else:
+            label_dict[l] += 1
+    numLabel = label_dict.__len__()
+    w = []
+    for i in range(numLabel):
+        w.append(classify(i + 1))
+    # with open("myLabel.txt") as f:
+    t = 0
+    for i in range(feature.shape[0]):
+        p = -numpy.inf
+        myLabel = -1
+        for j in range(numLabel):
+            q = p1(feature[i] * w[j])
+            print(j, q)
+            if p < q:
+                myLabel = j + 1
+                p = q
+        if label[i] == myLabel:
+            t += 1
+            print(myLabel)
+    print(t, t/feature.shape[0])
+
+
